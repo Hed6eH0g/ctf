5437030f90729ee8d353e623c7ab26968d8b65f4
Hed6eH0g <102146327+Hed6eH0g@users.noreply.github.com>
2023-05-25 23:05:05 +0900

    Add files via upload
    add files

---
+++ 2023/byuctf/misc/collision/solver.py
@@ -0,0 +1,74 @@
+# import base64
+# import hashlib
+# 
+# # get 2 base64-encoded strings from user (will only accept strings of length <= 10000)
+# str1 = input("Enter string 1: ")[:10000]
+# str2 = input("Enter string 2: ")[:10000]
+# 
+# # decode strings
+# pic1 = base64.b64decode(str1)
+# pic2 = base64.b64decode(str2)
+# 
+# # verify that both pictures are valid PNGs
+# if pic1[:8] != b'\x89PNG\r\n\x1a\n' or pic2[:8] != b'\x89PNG\r\n\x1a\n':
+#     print("Invalid PNG")
+#     quit()
+# 
+# # verify that both pictures have the same MD5 sum
+# if hashlib.md5(pic1).digest() != hashlib.md5(pic2).digest():
+#     print("MD5 sums do not match")
+#     quit()
+# 
+# # verify that base_str_1 is in one picture, and base_str_2 is in the other
+# base_str_1 = "Keeping your software and systems up-to-date with the latest security patches is a crucial step in safeguarding against potential cyber attacks."
+# base_str_2 = "Implementing strong passwords, two-factor authentication, and regular employee training are essential measures in maintaining a secure digital environment for your organization."
+# 
+# if (base_str_1.encode() in pic1 and base_str_2.encode() in pic2) or (base_str_2.encode() in pic1 and base_str_1.encode() in pic2):
+#     print("Strings found")
+#     print(open("flag.txt").read())
+# else:
+#     print("Strings not found")
+
+
+from pwn import *
+import hashlib
+import base64
+
+base_str_1 = "Keeping your software and systems up-to-date with the latest security patches is a crucial step in safeguarding against potential cyber attacks."
+base_str_2 = "Implementing strong passwords, two-factor authentication, and regular employee training are essential measures in maintaining a secure digital environment for your organization."
+
+txt1 = b'\x89PNG\r\n\x1a\n' + base_str_1.encode() + base_str_2.encode()
+# txt2 = b'\x89PNG\r\n\x1a\n'
+
+# i = 0
+# res = 0
+# while True:
+#     test1 = txt2 + base_str_2.encode() + str(i).encode()
+#     test2 = txt2 + str(i).encode() + base_str_2.encode()
+#     test3 = str(i).encode() + txt2 + base_str_2.encode()
+#     if hashlib.md5(txt1).digest() == hashlib.md5(test1).digest() :
+#         txt2 = test1
+#         break
+#     if hashlib.md5(txt1).digest() == hashlib.md5(test2).digest() :
+#         txt2 = test2
+#         break
+#     if hashlib.md5(txt1).digest() == hashlib.md5(test3).digest() :
+#         txt2 = test3
+#         break
+#     i += 1
+# 
+# print(txt1)
+# print(txt2)
+# print(hashlib.md5(txt1).digest())
+# print(hashlib.md5(txt2).digest())
+
+
+print(base64.b64encode(txt1))
+
+# io = remote('byuctf.xyz', 40016)
+# io.sendline(base64.b64encode(txt1))
+# io.sendline(base64.b64encode(txt1))
+# 
+# io.interactive()
+
+
